f46e2fa9fe7cef11280212df8a3152fe58307065
m-silver <mattsilver@windowslive.com>
2020-09-08 21:44:18 +1200

    Add more data to defects and defect_comments seed files

--- seeds/04-defects.js
+++ seeds/04-defects.js
@@ -17,7 +17,7 @@ exports.seed = (knex) => {
         },
         {
           id: 2, 
-          home_id: 2,
+          home_id: 1,
           trade: 'electrical',
           defect_name: 'sticky light switch',
           defect_description: 'sometimes the switch gets stuck in and we have to use a knife to jiggle loose',
@@ -27,10 +27,60 @@ exports.seed = (knex) => {
         },
         {
           id: 3, 
-          home_id: 3,
+          home_id: 1,
           trade: 'painting',
           defect_name: 'mark in paint',
           defect_description: '',
+          date_logged: knex.fn.now(),
+          status: 'complete',
+          image_path: ''
+        },
+        {
+          id: 4, 
+          home_id: 1,
+          trade: 'flooring',
+          defect_name: 'carpet stain',
+          defect_description: 'looks like paint',
+          date_logged: knex.fn.now(),
+          status: 'complete',
+          image_path: ''
+        },
+        {
+          id: 5, 
+          home_id: 2,
+          trade: 'plumbing',
+          defect_name: 'tap leaking',
+          defect_description: 'water keeps dripping, even when shut off fully',
+          date_logged: knex.fn.now(),
+          status: 'logged',
+          image_path: ''
+        },
+        {
+          id: 6, 
+          home_id: 2,
+          trade: 'electrical',
+          defect_name: 'sticky light switch',
+          defect_description: 'sometimes the switch gets stuck in and we have to use a knife to jiggle loose',
+          date_logged: knex.fn.now(),
+          status: 'in progress',
+          image_path: ''
+        },
+        {
+          id: 7, 
+          home_id: 2,
+          trade: 'painting',
+          defect_name: 'mark in paint',
+          defect_description: '',
+          date_logged: knex.fn.now(),
+          status: 'complete',
+          image_path: ''
+        },
+        {
+          id: 8, 
+          home_id: 2,
+          trade: 'flooring',
+          defect_name: 'carpet stain',
+          defect_description: 'looks like paint',
           date_logged: knex.fn.now(),
           status: 'complete',
           image_path: ''
--- seeds/05-defect_comments.js
+++ seeds/05-defect_comments.js
@@ -8,6 +8,9 @@ exports.seed = (knex) => {
         {id: 1, defect_id: 1, comment: 'it looks like the tap has been damaged by someone?'},
         {id: 2, defect_id: 2, comment: 'we have booked the electrician for Monday 10am'},
         {id: 3, defect_id: 3, comment: 'all finished, let us know if we can sign it off'},
+        {id: 4, defect_id: 4, comment: 'we looked but couldn\'t find the stain, could you be more specific please?'},
+        {id: 5, defect_id: 4, comment: 'it is just to the left of the kitchen island'},
+        {id: 6, defect_id: 4, comment: 'oh now I see it thanks!'},
       ])
     })
 }
